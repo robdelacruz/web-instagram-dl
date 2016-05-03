bcc7b9b5be21e453da0ff0c40bcb3a479985ea70
robdelacruz <robdelacruz@gmail.com>
2016-05-04 04:42:22 +0800

    Extract image now working

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 	var $src_url = $("#src_url");
 	var $btn_get = $("#btn_get");
 	var $btn_clear = $("#btn_clear");
+	var $target_img = $("#target_img");
 	var $target_contents = $('#target_contents');
 
 	$btn_get.click(function() {
@@ -17,25 +18,59 @@
 		var org_btn_get_val = $btn_get.val();
 		$btn_get.prop('disabled', true);
 		$btn_get.val('Wait...');
+		$btn_clear.prop('disabled', true);
 		$target_contents.text('Processing...');
 
 		var target_url = origin_url + encodeURIComponent(src_url_val) + "&callback=?";
 		$.getJSON(target_url, function(result) {
-			$target_contents.text(result.contents);
+			var image_url = extract_image_url(result.contents);
+			if (image_url) {
+				$target_contents.text('Image found');
+				$target_img.attr('src', image_url);
+			} else {
+				var not_found_msg = "No image found";
+				$target_contents.text(not_found_msg);
+				$target_img.attr('src', '');
+			}
 		})
-		.error(function() {
+		.fail(function() {
 			$target_contents.text('Error accessing url');
+			$target_img.attr('src', '');
 		})
-		.complete(function() {
+		.always(function() {
 			$btn_get.prop('disabled', false);
 			$btn_get.val(org_btn_get_val);
+			$btn_clear.prop('disabled', false);
 		});
 	});
 
 	$btn_clear.click(function() {
 		$src_url.val('');
 		$target_contents.text('');
+		$target_img.attr('src', '');
 	});
+
+	function extract_image_url(page_text) {
+		var regex = /<meta property="og:image" content="(.*?)" \/>/;
+		var match = regex.exec(page_text);
+		if (match == null || match.length < 2) {
+			// Image url not found
+			return null;
+		}
+
+		var image_url = match[1];
+		return image_url;
+	}
+
+	/*
+	var s_test = "abcdefghi";
+	var regex = /ab(c)(def\w+)/m;
+
+	var match = regex.exec(s_test);
+	for (var i=0; i < match.length; i++) {
+		console.log("match[" + i.toString() + "] = " + match[i]);
+	}
+	*/
 
 })();
 
